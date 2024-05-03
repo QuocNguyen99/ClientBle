4fa90a85a43e4a8f60ce096f0f8603f8ffb6a55e
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2024-05-04 00:43:48 +0700

    UI for connecting to device

--- app/src/main/java/com/example/bleclient/BlePage.kt
+++ app/src/main/java/com/example/bleclient/BlePage.kt
@@ -2,25 +2,43 @@ package com.example.bleclient
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import java.nio.ByteOrder
 import java.util.Calendar
@@ -30,43 +48,67 @@ import java.util.Date
 @SuppressLint("MissingPermission")
 @Composable
 fun BlePage(viewModel: MainViewModel, hasRequiredBluetoothPermissions: () -> Boolean, requestRelevantRuntimePermissions: () -> Unit) {
-    val deviceSan by viewModel.deviceScan.collectAsState()
+    Column {
+        val pageCount = 10
+        val pagerState = rememberPagerState(
+            pageCount = { pageCount },
+        )
 
-    Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Column(modifier = Modifier.fillMaxSize()) {
-            LazyColumn(modifier = Modifier
-                .weight(1f),
-                content = {
-                    items(deviceSan) {
-                        ListItem(
-                            headlineContent = { Text(text = it.name ?: " unknown") },
-                            supportingContent = { Text(text = it.address) },
-                            modifier = Modifier.combinedClickable(
-                                onClick =  { viewModel.connectToDevice(it) },
-                                onLongClick = {
-                                    viewModel.writeCharacteristic()}))
-                    }
-                })
-
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Button(modifier = Modifier.fillMaxWidth(0.5f), onClick = {
-                    if (!hasRequiredBluetoothPermissions()) {
-                        requestRelevantRuntimePermissions()
-                    } else {
-                        viewModel.scanBle()
-                    }
-
-                }) {
-                    Text(text = "Scan")
-                }
-                Button(modifier = Modifier.fillMaxWidth(1f), onClick = { viewModel.stopBle() }) {
-                    Text(text = "Stop")
-                }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+        ) {
+            when (it) {
+                0 -> ConnectingDeviceScreen()
+                1 -> {}
+                2 -> {}
             }
         }
 
-        Dialog(onDismissRequest = { }) {
-
+        Row(
+            Modifier
+                .height(50.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(pageCount) { iteration ->
+                val color = if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .background(color, CircleShape)
+                        .size(12.dp)
+                )
+            }
         }
+    }
+}
+
+@Composable
+fun ConnectingDeviceScreen(deviceName: String = "Device name") {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Connecting to device", style = MaterialTheme.typography.bodyLarge, fontSize = 20.sp)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = "",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.35f),
+
+            )
+
+        Spacer(modifier = Modifier.height(24.dp))
+        Text(text = "Connecting to $deviceName", style = MaterialTheme.typography.bodyLarge, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(text = "Make surre your phone or tablet is near your $deviceName so they can connect", style = MaterialTheme.typography.bodyLarge)
     }
 }
